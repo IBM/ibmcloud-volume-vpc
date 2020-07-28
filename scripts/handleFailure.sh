fd3120d07fdb97af2b0c95d6ac2f7a0df0ff5e0c
Meghna Singh <Meghna.Singh1@ibm.com>
2020-07-28 16:29:01 +0530

    change repo name in scripts

--- scripts/handleFailure.sh
+++ scripts/handleFailure.sh
@@ -9,11 +9,11 @@
 # *****************************************************************************/
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_GO_VERSION" == "tip" ]; then
-	curl -s -k -X GET -H "Content-Type: application/json" -H "Accept: application/vnd.travis-ci.2+json"  -H "Authorization: token $TRAVIS_TOKEN"  https://travis-ci.com/IBM/ibmcloud-storage-volume-lib/builds/$TRAVIS_BUILD_ID | jq '.jobs[0].state' | sed 's/"//g'> state.out
+	curl -s -k -X GET -H "Content-Type: application/json" -H "Accept: application/vnd.travis-ci.2+json"  -H "Authorization: token $TRAVIS_TOKEN"  https://travis-ci.com/IBM/ibmcloud-volume-vpc/builds/$TRAVIS_BUILD_ID | jq '.jobs[0].state' | sed 's/"//g'> state.out
 	RESULT=$(<state.out)
 	if [ "$RESULT" != "failed" ]; then
 		RESULT_MESSAGE=":warning: Build failed with **tip** version."
-		curl -X POST -H "Authorization: token $GHE_TOKEN" https://github.com/IBM/ibmcloud-storage-volume-lib/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments -H 'Content-Type: application/json' --data '{"body": "'"$RESULT_MESSAGE"'"}'
+		curl -X POST -H "Authorization: token $GHE_TOKEN" https://github.com/IBM/ibmcloud-volume-vpc/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments -H 'Content-Type: application/json' --data '{"body": "'"$RESULT_MESSAGE"'"}'
 	fi
 fi
 exit 0
