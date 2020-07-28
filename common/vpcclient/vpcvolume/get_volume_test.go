c2adc9e96d2fc65762178f178fa6e4a0f71e9a32
Meghna Singh <Meghna.Singh1@ibm.com>
2020-07-28 16:23:24 +0530

    rebase

--- common/vpcclient/vpcvolume/get_volume_test.go
+++ common/vpcclient/vpcvolume/get_volume_test.go
@@ -1,25 +1,32 @@
-/*******************************************************************************
- * IBM Confidential
- * OCO Source Materials
- * IBM Cloud Container Service, 5737-D43
- * (C) Copyright IBM Corp. 2018 All Rights Reserved.
- * The source code for this program is not  published or otherwise divested of
- * its trade secrets, irrespective of what has been deposited with
- * the U.S. Copyright Office.
- ******************************************************************************/
+/**
+ * Copyright 2020 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package vpcvolume_test
 
 import (
+	"net/http"
+	"os"
+	"testing"
+
 	"github.com/IBM/ibmcloud-volume-vpc/common/vpcclient/models"
 	"github.com/IBM/ibmcloud-volume-vpc/common/vpcclient/riaas/test"
 	"github.com/IBM/ibmcloud-volume-vpc/common/vpcclient/vpcvolume"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"net/http"
-	"os"
-	"testing"
 )
 
 func GetTestContextLogger() (*zap.Logger, zap.AtomicLevel) {
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/IBM/ibmcloud-volume-vpc
 
-go 1.14
+go 1.13.8
 
 require (
 	
