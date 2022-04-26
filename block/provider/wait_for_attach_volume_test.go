9b750c7836fe79c248d2f323c0dfdf7025fadc91
Guna K Kambalimath <Guna.Kambalimath@ibm.com>
2022-04-26 14:47:23 +0530

    Updating UT

--- block/provider/wait_for_attach_volume_test.go
+++ block/provider/wait_for_attach_volume_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/IBM/ibmcloud-volume-interface/lib/provider"
 	util "github.com/IBM/ibmcloud-volume-interface/lib/utils"
 	"github.com/IBM/ibmcloud-volume-interface/lib/utils/reasoncode"
+	userError "github.com/IBM/ibmcloud-volume-vpc/common/messages"
 	volumeAttachServiceFakes "github.com/IBM/ibmcloud-volume-vpc/common/vpcclient/instances/fakes"
 	"github.com/IBM/ibmcloud-volume-vpc/common/vpcclient/models"
 	"github.com/stretchr/testify/assert"
@@ -169,6 +170,7 @@ func TestWaitForAttachVolume(t *testing.T) {
 }
 
 func TestWaitForAttachVolumeForInvalidSession(t *testing.T) {
+	userError.MessagesEn = userError.InitMessages()
 	logger, teardown := GetTestLogger(t)
 	defer teardown()
 	vpcs, uc, sc, err := GetTestOpenInvalidSession(t, logger)
@@ -176,11 +178,12 @@ func TestWaitForAttachVolumeForInvalidSession(t *testing.T) {
 	assert.NotNil(t, uc)
 	assert.NotNil(t, sc)
 	assert.Nil(t, err)
-	//expectedError := "{Code:InvalidServiceSession, Type:RetrivalFailed, Description:The Service Session was not found due to error while generating IAM token., BackendError:IAM token exchange request failed, RC:500}"
+	expectedError := "{Code:InvalidServiceSession, Type:RetrivalFailed, Description:The Service Session was not found due to error while generating IAM token., BackendError:IAM token exchange request failed, RC:500}"
 	volumeAttachRequest := provider.VolumeAttachmentRequest{
 		VolumeID: "vol-1",
 	}
 
 	_, err = vpcs.WaitForAttachVolume(volumeAttachRequest)
 	assert.NotNil(t, err)
+	assert.Equal(t, expectedError, err.Error())
 }
