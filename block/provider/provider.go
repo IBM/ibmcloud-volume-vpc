7cff236f3e7df182ad064cc0be45a229f441ebbf
Guna K Kambalimath <Guna.Kambalimath@ibm.com>
2023-01-18 12:49:41 +0530

    Addressing comments

--- block/provider/provider.go
+++ block/provider/provider.go
@@ -90,26 +90,20 @@ func NewProvider(conf *vpcconfig.VPCBlockConfig, spObject sp.SecretProviderInter
 	conf.VPCConfig.APIKey = conf.VPCConfig.G2APIKey
 	conf.VPCConfig.ResourceGroupID = conf.VPCConfig.G2ResourceGroupID
 
-	//Set API Generation As 2 (if unspecified in config/ENV-VAR)
-	if conf.VPCConfig.G2VPCAPIGeneration <= 0 {
-		conf.VPCConfig.G2VPCAPIGeneration = NEXTGenProvider
-	}
-	conf.VPCConfig.VPCAPIGeneration = conf.VPCConfig.G2VPCAPIGeneration
+	//Set API Generation As 2
+	conf.VPCConfig.G2VPCAPIGeneration = NEXTGenProvider
+	conf.VPCConfig.VPCAPIGeneration = NEXTGenProvider
 
 	//Set the APIVersion Date, it can be different in GC and NG
 	if conf.VPCConfig.G2APIVersion != "" {
 		conf.VPCConfig.APIVersion = conf.VPCConfig.G2APIVersion
 	}
 
-	//set provider-type (this usually comes from the secret)
-	if conf.VPCConfig.VPCBlockProviderType != VPCNextGen {
-		conf.VPCConfig.VPCBlockProviderType = VPCNextGen
-	}
+	//set provider-type
+	conf.VPCConfig.VPCBlockProviderType = VPCNextGen
 
 	//Mark this as enabled/active
-	if conf.VPCConfig.VPCTypeEnabled != VPCNextGen {
-		conf.VPCConfig.VPCTypeEnabled = VPCNextGen
-	}
+	conf.VPCConfig.VPCTypeEnabled = VPCNextGen
 
 	contextCF, err := vpcauth.NewVPCContextCredentialsFactory(conf, spObject)
 	if err != nil {
